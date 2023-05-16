2c6bcf46dccc586930c6d20d30378f985711f164
Zach <35601976+Zak223@users.noreply.github.com>
2023-05-16 13:24:29 -0400

    Update ultrasonic-reader.ino

--- tinkercad/ultrasonic-reader.ino
+++ tinkercad/ultrasonic-reader.ino
@@ -1,19 +1,18 @@
-int inches = 0;
+int inches = 0; //Declare the inches variable
 
-int cm = 0;
+int cm = 0; //Declare the cm variable
 
-long readUltrasonicDistance(int triggerPin, int echoPin)
+long readUltrasonicDistance(int triggerPin, int echoPin) //Create a 
 {
   pinMode(triggerPin, OUTPUT);  // Clear the trigger
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigger pin to HIGH state for 10 microseconds
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
-  pinMode(echoPin, INPUT);
+  digitalWrite(triggerPin, LOW); // Set the trigger pin to low
+  delayMicroseconds(2); // Delay for 2 microseconds
+  digitalWrite(triggerPin, HIGH); //Set the trigger pin to high
+  delayMicroseconds(10); // Delay for 10 microseconds
+  digitalWrite(triggerPin, LOW); // Set the trigger pin to low
+  pinMode(echoPin, INPUT); // Set the echoPin to input
   // Reads the echo pin, and returns the sound wave travel time in microseconds
-  return pulseIn(echoPin, HIGH);
+  return pulseIn(echoPin, HIGH); // Waits until the pin gets a high signal then writes the distance
 }
 
 void setup()
