c54608265f72da3a50b9300982d0968ac883de45
Zach <35601976+Zak223@users.noreply.github.com>
2023-06-01 14:40:25 -0400

    Update fullcode.ino
    Code Rewrite

--- full-code/fullcode.ino
+++ full-code/fullcode.ino
@@ -1,52 +1,126 @@
-char t = '1'; //Set t to equal 1
-char appData;  //Define appData to char
-String inData = ""; //Set inData to ""
- #include <SoftwareSerial.h> //Include SoftwareSerial.h library
-void setup() { //Code that runs once at the begining of runtime
-  Serial.begin(9600); //Start serial at 9600 baud
+#include <Servo.h>
+Servo myServo;
+int state = 0;
+int servoVal = 0;
+const int motorPin1 = 13; // Pin 15 of L293
+const int motorPin2 = 12; // Pin 10 of L293
+//Right Motor Connections
+const int motorPin3 = 11; // Pin 7 of L293
+const int motorPin4 = 10;
+void setup() 
+{
+  myServo.attach(12);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Serial Started");
 }
- 
-void loop() { //Constantly runs 
-if(Serial.available()){ //If serial is available
-  t = Serial.read(); // Set t equal to the output of Serial
-  Serial.println(t);  //Print t in serial
+
+void loop() 
+{
+  if (Serial.available()>0)
+  { // Checks whether data is coming from the serial port
+    state = Serial.read(); // Reads the data from the serial port
+    Serial.println(state);
+    switch (state)
+    { //switch statement easily deals with multiple results
+      case 'F': //Program how to respond to each case
+      FWD(); break; //if you don’t program a response to
+      case 'B': //a button it will be ignored
+      BKWD(); break;
+      case 'L':
+      SpinL(); break;
+      case 'R':
+      SpinR(); break;
+      case 'S':
+      SoftSTOP(); break; 
+      case 'G':
+      FWDL(); break;
+      case 'I':
+      FWDR(); break;
+      case 'H':
+      BKWDL(); break;
+      case 'J':
+      BKWDR(); break;
+      case 'V':
+      hornON(); break;
+      case 'v':
+      hornOFF(); break;
+      case 'X':
+      axeON(); break;
+      case 'x':
+      axeOFF(); break;
+    }
+    delay(1);
+  }
+  else
+  {
+    delay(100);
+    SoftSTOP();
+  }
 }
- 
-if(t == '1'){            //If the serial outputs 1
-  digitalWrite(13,HIGH); //Write motor to high
-  digitalWrite(12,LOW);//Write motor to low
-  digitalWrite(11,HIGH);//Write motor to high
-  digitalWrite(10,LOW);//Write motor to low
- 
- //Doing so makes the car go forwards
-}
- 
-else if(t == '2'){      //If the serial outputs 2
-  digitalWrite(13,LOW); // Write all 
-  digitalWrite(12,HIGH);
-  digitalWrite(11,LOW);
-  digitalWrite(10,HIGH);
-}
- 
-else if(t == '3'){      //turn right (left side motors rotate in forward direction, right side motors doesn't rotate)
-  digitalWrite(13,LOW);
+void FWD()
+{
+  digitalWrite(13,HIGH);
   digitalWrite(12,LOW);
   digitalWrite(11,HIGH);
   digitalWrite(10,LOW);
 }
- 
-else if(t == '4'){      //turn left (right side motors rotate in forward direction, left side motors doesn't rotate)
-  digitalWrite(13,HIGH);
-  digitalWrite(12,LOW);
-  digitalWrite(11,LOW);
-  digitalWrite(10,LOW);
+void BKWD()
+{
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, HIGH);
 }
- 
-else if(t == '5'){      //STOP (all motors stop)
-  digitalWrite(13,LOW);
-  digitalWrite(12,LOW);
-  digitalWrite(11,LOW);
-  digitalWrite(10,LOW);
+void SoftSTOP()
+{
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
 }
-delay(100);
+void SpinL()
+{
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, HIGH);
+}
+void SpinR()
+{
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+}
+void FWDL()
+{
+  analogWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  analogWrite(motorPin3, 100);
+  analogWrite(motorPin4, LOW);
+}
+void FWDR()
+{
+  analogWrite(motorPin1, LOW);
+  analogWrite(motorPin2, 100);
+  digitalWrite(motorPin3, HIGH);
+  analogWrite(motorPin4, LOW);
+}
+void BKWDL()
+{
+  analogWrite(motorPin3, LOW);
+  analogWrite(motorPin4, 100);
+  digitalWrite(motorPin1, HIGH);
+  analogWrite(motorPin2, LOW);
+}
+void BKWDR()
+{
+  analogWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, HIGH);
+  analogWrite(motorPin1, 100);
+  analogWrite(motorPin2, LOW);
 }
